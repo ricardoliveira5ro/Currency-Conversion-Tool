583e29f7e6cb334496cfc78c93c7a832b969de1a
Ricardo Oliveira <201901561@estudantes.ips.pt>
2023-10-21 16:20:14 +0100

    header completed

--- src/app/page.js
+++ src/app/page.js
@@ -4,27 +4,39 @@ import '../styles/styles.css'
 export default function Home() {
   return (
     <>
-    <header className='header'>
-      <div className='header-name'>
-        <h1>curren</h1>
-        <p>See</p>
-      </div>
-      <div className='header-side'>
-        <Image
-          src="/github-sign.svg"
-          alt="GitHub Logo"
-          width={40}
-          height={10}
-          priority
-        />
-        <div className='header-feedback'>
-          <p>Feedback</p>
+    <section className='page'>
+      <header className='header'>
+        <div className='header-name'>
+          <h1>curren</h1>
+          <p>See</p>
         </div>
-      </div>
-    </header>
-    <main>
-
-    </main>
+        <div className='header-side'>
+          <Image
+            src="/github-sign.svg"
+            alt="GitHub Logo"
+            width={44}
+            height={10}
+            priority
+          />
+          <div className='header-feedback'>
+            <Image
+              src="/star.svg"
+              alt="Star icon"
+              width={17}
+              height={5}
+              priority
+            />
+            <p>Feedback</p>
+          </div>
+        </div>
+      </header>
+      <main className='main'>
+        <div className='main-title'>
+          <h1>Currency conversion tool</h1>
+          <h3>Check live currency exchange rates</h3>
+        </div>
+      </main>
+    </section>
     </>
   )
 }
